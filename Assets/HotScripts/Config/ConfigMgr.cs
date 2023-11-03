0cf5413234f6ae7964c5377b40d9ca6bb8ca3984
wangdebin <wangdebin@merryrealm.com>
2023-11-03 16:21:26 +0800

    LUBAN

---
+++ Assets/HotScripts/Config/ConfigMgr.cs
@@ -0,0 +1,47 @@
+﻿using Game.Util;
+using Cysharp.Threading.Tasks;
+using UIFramework;
+using YooAsset;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Luban;
+
+namespace Game.UI
+{
+    public class ConfigMgr : SingletonBase<ConfigMgr>
+    {
+        private bool m_IsInit = false;
+        private Engine m_Engin;
+        private Game.Data.GameConfigs m_GameConfigs;
+        public async UniTask Init(Engine engine)
+        {
+            m_Engin = engine;
+            m_GameConfigs = new Game.Data.GameConfigs();
+            await m_GameConfigs.LoadRes(LoadByteBuf);
+        }
+        public void Destroy()
+        {
+            if (!m_IsInit)
+            {
+                return;
+            }
+        }
+        public void Update(float dt)
+        {
+            if (!m_IsInit)
+            {
+                return;
+            }
+        }
+
+        private static async UniTask<ByteBuf> LoadByteBuf(string file)
+        {
+            AssetHandle byteBufData = YooAssets.LoadAssetAsync<TextAsset>(file);
+            await byteBufData.Task;
+            return new ByteBuf(((TextAsset)byteBufData.AssetObject).bytes);
+        }
+
+    }
+}
+
